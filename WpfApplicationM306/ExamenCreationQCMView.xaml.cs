ff8a93610b500d83a3c56ab179ae2b6f62384077
Nathan Tornay <NathTorn1@edueptm.ch>
2016-11-17 19:33:20 +0100

    Commit des premieres vues (Non-fonctionnel) pour mockup uniquement

--- WpfApplicationM306/ExamenCreationQCMView.xaml.cs
+++ WpfApplicationM306/ExamenCreationQCMView.xaml.cs
@@ -10,28 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApplicationM306
 {
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Logique d'interaction pour ExamenCreationQCMView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ExamenCreationQCMView : Window
     {
-        public MainWindow()
+        public ExamenCreationQCMView()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            var t = new TypeExamenView();
-            t.ShowDialog();
-
-            var test = new ExamenCreationQCMView();
-            test.ShowDialog();
         }
     }
 }
--- WpfApplicationM306/TypeExamenView.xaml.cs
+++ WpfApplicationM306/TypeExamenView.xaml.cs
@@ -10,28 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApplicationM306
 {
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Logique d'interaction pour TypeExamenView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TypeExamenView : Window
     {
-        public MainWindow()
+        public TypeExamenView()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            var t = new TypeExamenView();
-            t.ShowDialog();
-
-            var test = new ExamenCreationQCMView();
-            test.ShowDialog();
         }
     }
 }
